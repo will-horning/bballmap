689afa7a3f82bfd69b1b0e2eac1c99366a1c1e48
willhorning <will.horning@gmail.com>
2013-11-25 16:56:45 -0500

    added script to detect and relabel mislabelled free throws

--- freethrowfinder.py
+++ freethrowfinder.py
@@ -12,23 +12,19 @@ players = session.query(Player).all()
 for p in players:
 	print p
 	n_freethrows = 0
-	shots = session.query(Shot).filter_by(player_id = p.id).all()
+	shots = session.query(Shot).filter_by(player_id = p.id).order_by(Shot.datetime).all()
 	if len(shots) < 400: continue
 	for i, shot in enumerate(shots[1:-2]):
 		prev_shot = shots[i-1]
 		next_shot = shots[i+1]
-		if prev_shot.time == shot.time:
+		if prev_shot.datetime == shot.datetime:
+			print prev_shot.datetime
+			print shot.datetime
 			n_freethrows += 1
 			session.add(shot)
 			session.add(prev_shot)
 			shot.shot_type = "Free Throw"
 			prev_shot.shot_type = "Free Throw"
-		if next_shot.time == shot.time:
-			n_freethrows += 1
-			session.add(shot)
-			session.add(next_shot)
-			shot.shot_type = "Free Throw"
-			next_shot.shot_type = "Free Throw"
    	print n_freethrows
 	print len(shots)
 session.commit()
