96dbbf6229b6108b499e24191f56801f36bc4bcc
willhorning <will.horning@gmail.com>
2013-12-07 19:31:52 -0500

    changed interface to use selects, cleaned up heatmap.js

--- static/heatmap.js
+++ static/heatmap.js
@@ -7,67 +7,32 @@ function(){
 	var selected_player_list = [];
 	var td = [];
 
-
-	list_remove = function(a, k){
-		newa = [];
-		for(var i in a){
-			if(a[i] != k){
-				newa.push(a[i]);
-			}
-		}
-		return newa;
-	}
-
-	update_players = function(){
-		alert(selected_players.length);
-		sdivs = "";
-		for(i = 0; i < selected_players.length; i++){
-			player = selected_players[i];
-			link = "<a href=\"#\">" + player.name + "</a>";
-			sdivs += make_simple_div("player" + player.id,
-									"playerlink", link );
-		}
-		$("#selectedplayers").html(sdivs);
-	}
-
-	make_simple_div = function(id, divclass, content){
-		tag =  "<div id='" + id + "' class='" + divclass + "'>";
-		tag += content + "</div>";
-		return tag;
-	}
-
 	$.getJSON("get_teams", {}, function(data){
-		divs = [];
 		teams = data["teams"];
 		for(i=0; i < teams.length; i++){
 			team = teams[i];
-			link = "<a href=\"#\">" + team.name + "</a>";
-			divs += make_simple_div("team" + team.id,
-									"teamlink", link );
+			$('#teamselect').append('<option value="' + 'team' + team.id + '">' + team.name + '</option>');
 		}
-		$('#teamselect').append(divs);
-		activate_tselect();
 	});
-
-	activate_tselect = function(){
-		$('div.teamlink').click(function(){
-			$.getJSON("get_players", {"team_id": this.id}, function(data){
-				divs = [];
+	
+	get_players = function(){
+		$.getJSON("get_players", {"team_id": $("#teamselect option:selected").val()}, 
+			function(data){
+				$("#playerselect").html('<option value="None"> Choose a player: </option>');
 				players = data["players"];
 				for(i=0; i < players.length; i++){
 					player = players[i];
-					link = "<a href=\"#\">" + player.name + "</a>";
-					divs += make_simple_div("player" + player.id,
-											"playerlink", link );
+					$("#playerselect").append('<option value="' + player.id + '">' + player.name + '</option>');
 				}
 				selected_player_list = [];
 				$('#heatmapdiv').html("");
-				$('#selectedplayers').html("");
-				$('#unselectedplayers').html(data["teamname"] + divs);
-				activate_pselect();
+				$("#playerselect").show();
 			});
-		});
+	};
 
+	choose_player = function(){
+		selected_player_list.push($("#playerselect option:selected").val())
+		update_heatmap_img();
 	};
 
 	strip_id = function(idstr, index){
@@ -76,38 +41,6 @@ function(){
 			justid += idstr[i];
 		}
 		return justid;
-	};
-
-	inlist = function(list, item){
-		for(i=0; i < list.length; i++){
-			if(list[i] == item){ return true;}
-		}
-		return false;
-	};
-
-	activate_pselect = function(){
-		$('div.playerlink').click(function(){
-			name = "#" + this.id;
-			pid = strip_id(this.id, 6);
-			link = $(name).html();
-			div = make_simple_div("player" + pid,
-								  "playerlink", link);
-			if(inlist(selected_player_list, pid)){
-				selected_player_list = list_remove(selected_player_list, parseInt(pid));
-
-				$(name).remove();
-				$("#unselectedplayers").append(div);
-				activate_pselect();
-			}
-			else{
-				selected_player_list.push(parseInt(pid));
-
-				$(name).remove();
-				$("#selectedplayers").append(div);
-				activate_pselect();
-			}
-			update_heatmap_img();
- 		});
 	};
 
 	update_heatmap_img = function(){
