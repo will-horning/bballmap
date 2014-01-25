3704a17db083c03918c570e8a22a6170e1e47d8f
willhorning <will.horning@gmail.com>
2014-01-25 15:18:14 -0500

    using refactored scraper, using requests instead of urllib2

--- cbs_scraper.py
+++ cbs_scraper.py
@@ -1,80 +1,59 @@
-import datetime, urllib2, re
-from sqlalchemy import *
+from models import *
 from sqlalchemy.orm import sessionmaker
-from models import Player, Shot, Team
+from sqlalchemy import create_engine
+from sqlalchemy import *
+
+import datetime, re, requests
 
 Session = sessionmaker()
-db = create_engine("sqlite:///shots.db")
+db = create_engine("sqlite:///shots2.db", 
+                    connect_args={'check_same_thread':False})
 Session.configure(bind=db)
 session = Session()
 
-base_scoreboard_url = "http://www.cbssports.com/nba/scoreboard/"
-scoreboard_urls = []
+cbs_url = "http://www.cbssports.com"
+ncaa_url = "http://www.cbssports.com/collegebasketball/scoreboard/div1/"
+nba_url = "http://www.cbssports.com/nba/scoreboard/"
+date = datetime.date.today()
+n_years = 1
+start_urls = []
+for k in xrange(128):
+    url = nba_url + str(date - datetime.timedelta(days=k))
+    start_urls.append(url.replace('-',''))
 
-x=0
-current_date = datetime.date.today()
-while current_date.year > 2009:
-	x+=1
-	scoreboard_urls.append(base_scoreboard_url + str(current_date).replace("-", ""))	
-	current_date -= datetime.timedelta(days=1)
-gametracker_urls = set()
+def get_gametracker_urls(schedule_url):
+    html = requests.get(schedule_url).text
+    urls = re.findall(r'href=[\'"]?([^\'" >]+)', html)
+    return (cbs_url + url for url in urls if 'gametracker/live' in url)
 
-for url in scoreboard_urls:
-	try:
-		html = urllib2.urlopen(url).read()
-		links = re.findall('''href=["'](.[^"']+)["']''', html)	
-		gametracker_urls |= set(["http://www.cbssports.com" + l for l in links if "gametracker/live" in l])
-	except urllib2.HTTPError:
-		continue
-	
-def extract_shotdata(html):
-	date_string = re.search(r'NBA_(\d{8})', html).group(1)
-	shot_data = re.search(r'shotData: "(.*?)\s', html).group(1)
-	if shot_data:
-		for shot_data_string in shot_data.strip().split("~"):
-			session.add(Shot(shot_data_string, date_string))
-	else:
-		f = open("foo.txt", "w")
-		f.write(html)
-		f.close()
-		assert False
-		
-def extract_player_ids(html):
-	away_data = re.search(r'awayScoringData: "(.*?)"', html).group(1).split("|")
-	home_data = re.search(r'homeScoringData: "(.*?)"', html).group(1).split("|")
-	away_team_name, home_team_name = re.search(r'[->] ?(.*?) vs. (.*?) -', html).groups()
+def get_shot_data(game_url):
+    html = requests.get(game_url).text
+    awaydata = re.search(r'awayScoringData: "(.*?)"', html).group(1).split("|")
+    homedata = re.search(r'homeScoringData: "(.*?)"', html).group(1).split("|")
+    awayname, homename = re.search(r'[->] ?(.*?) vs. (.*?) -', html).groups()
+    get_team_data(awayname, awaydata)
+    get_team_data(homename, homedata)
+    date_string = re.search(r'NBA_(\d{8})', html).group(1)
+    shot_data = re.search(r'shotData: "(.*?)\s', html).group(1)
+    for shot_data_string in shot_data.strip().split("~"):
+        if shot_data_string:
+            try: 
+                session.add(Shot(shot_data_string, date_string))
+            except: print shot_data_string
+        else: print shot_data_string
+        
+def get_team_data(team_name, player_data):
+    t = session.query(Team).filter_by(name=team_name).first()
+    if not t: 
+        t = Team(team_name)
+        session.add(t)
+    for s in player_data:
+        player_id, p_name = re.search(r'(\d*):(.*?),', s).groups()
+        if not session.query(Player).filter_by(id=player_id).first():
+            # if "\xa0" in p_name: p_name = p_name.replace("\xa0", " ")
+            if "&nbsp" in p_name: p_name = p_name.replace("&nbsp;", " ")
+            session.add(Player(player_id, p_name, t.id))
 
-	def f(team_name, player_data):
-		player_team = session.query(Team).filter(Team.name == team_name).first()
-		if not player_team:
-			session.add(Team(team_name))
-			player_team = session.query(Team).filter(Team.name == team_name).first()
-		for s in player_data:
-			player_id, player_name = re.search(r'(\d*):(.*?),', s).groups()
-			player_id = int(player_id)
-			if "\xa0" in player_name: player_name = player_name.replace("\xa0", " ")
-			if "&nbsp" in player_name: player_name = player_name.replace("&nbsp;", " ").split(" ", 1)
-			if len(player_name) == 1:
-				fname = player_name[0]
-				lname = ""
-			else:
-				fname, lname = player_name[0], "".join(player_name[1:])
-			if not session.query(Player).get(player_id):
-				session.add(Player(player_id, fname, lname, player_team.id))
-	f(away_team_name, away_data)
-	f(home_team_name, home_data)
-
-while gametracker_urls:
-	url = gametracker_urls.pop()
-	try:
-		tml = urllib2.urlopen(url).read()
-		extract_shotdata(tml)
-		extract_player_ids(tml)
-	except urllib2.URLError:
-		print url
-	print len(gametracker_urls)
-									                  
-session.commit()
-
-
-
+for start_url in start_urls:
+    for game_url in get_gametracker_urls(start_url):
+        get_shot_data(game_url)
